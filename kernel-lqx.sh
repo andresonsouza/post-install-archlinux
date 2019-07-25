b44a8cad14928648aa4b23327a95343a91a4abf9
andresonsouza <andresonjazz@gmail.com>
2019-07-25 14:15:13 -0300

    Script de instalação do kernel Liquorix

---
+++ kernel-lqx.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+## Instalação do Kernel low latency para melhorar o desempenho e trabalhar com audio
+
+yay -S linux-lqx linux-lqx-headers virtualbox-host-modules-lqx linux-lqx-docs
+
